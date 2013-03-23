b7ffd152d64ab60eeee946a93e1668f70ce8f6b8
John West <john.sheffield.west@gmail.com>
2013-03-23 11:22:21 -0400

    Cleans up the routes in app.js. Changes the version number. Changes the styling to make, essentially, two columns.

--- app.js
+++ app.js
@@ -30,22 +30,6 @@ app.get( '/sections/:id', function( req, res) {
   var id = req.params.id;
   res.send( data.article[id] );
 });
-/*
-app.get( '/art/:section_id/:id', function( req, res ) {
-  var id = parseInt( req.params.id );
-  var section_id = parseInt( req.params.section_id );
-  var number_of_paragraphs = 0;
-  var actual_art_limit = data.art.length;
-  for( var i = 0; i <= section_id; i++ ) {
-    console.log('looping');
-    if( id + number_of_paragraphs < actual_art_limit && i == section_id ) {
-      res.send( data.art[ id + number_of_paragraphs] );
-    }
-    number_of_paragraphs = number_of_paragraphs + data.article[i].body.length;
-  }
-});
-*/
-
 
 http.createServer( app ).listen( app.get( 'port' ), function() {
   console.log( "Express server listening on port " + app.get( 'port' ) );
